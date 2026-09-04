a27273dc809bee8c6c0c6b3c562ef7e19f7e4afc
DivCodexweb <divcodexweb@gmail.com>
2026-09-04 12:03:42 +0530

    pusing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import Aoscompo from "@/utils/aos";
-const dmsans = DM_Sans({ subsets: ["latin"] });
 import NextTopLoader from 'nextjs-toploader';
 import { AppContextProvider } from "../context-api/PropertyContext";
 import Footer from "./components/layout/footer";
@@ -11,32 +10,33 @@ import ScrollToTop from "./components/scroll-to-top";
 import Header from "./components/layout/header";
 import SessionProviderComp from "./provider/SessionProviderComp";
 
+const dmsans = DM_Sans({ subsets: ["latin"] });
+
 export default function RootLayout({
   children,
-  session,
 }: Readonly<{
   children: React.ReactNode;
-  session:any
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${dmsans.className}`}>
-      <AppContextProvider>
-      <SessionProviderComp session={session}>
-        <ThemeProvider
-          attribute="class"
-          enableSystem={false}
-          defaultTheme="light"
-        >
-          <Aoscompo>
-            <Header />
-            <NextTopLoader />
-            {children}
-            <Footer />
-          </Aoscompo>
-          <ScrollToTop />
-        </ThemeProvider>
-        </SessionProviderComp>
+        <AppContextProvider>
+          {/* Omit passing session here; let SessionProviderComp handle session fetching via auth() or Client side */}
+          <SessionProviderComp>
+            <ThemeProvider
+              attribute="class"
+              enableSystem={false}
+              defaultTheme="light"
+            >
+              <Aoscompo>
+                <Header />
+                <NextTopLoader />
+                {children}
+                <Footer />
+              </Aoscompo>
+              <ScrollToTop />
+            </ThemeProvider>
+          </SessionProviderComp>
         </AppContextProvider>
       </body>
     </html>
--- src/app/provider/SessionProviderComp.tsx
+++ src/app/provider/SessionProviderComp.tsx
@@ -1,17 +1,11 @@
-"use client";
-import { SessionProvider } from "next-auth/react";
-import React from "react";
+'use client';
 
-export default function SessionProviderComp({
-  children,
-  session,
-}: {
-  children: React.ReactNode;
-  session: any;
-}) {
+import { SessionProvider } from "next-auth/react";
+
+export default function SessionProviderComp({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <SessionProvider session={session}>{children}</SessionProvider>
-    </>
+    <SessionProvider>
+      {children}
+    </SessionProvider>
   );
 }
