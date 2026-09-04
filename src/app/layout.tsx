ea7667635d815d4379b5c478bb7d39aaa9681836
DivCodexweb <divcodexweb@gmail.com>
2026-09-04 11:47:04 +0530

    Fix layout props error for Vercel deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import Aoscompo from "@/utils/aos";
-const dmsans = DM_Sans({ subsets: ["latin"] });
 import NextTopLoader from 'nextjs-toploader';
 import { AppContextProvider } from "../context-api/PropertyContext";
 import Footer from "./components/layout/footer";
@@ -10,32 +9,32 @@ import ScrollToTop from "./components/scroll-to-top";
 import Header from "./components/layout/header";
 import SessionProviderComp from "./provider/SessionProviderComp";
 
+const dmsans = DM_Sans({ subsets: ["latin"] });
+
 export default function RootLayout({
   children,
-  session,
 }: Readonly<{
   children: React.ReactNode;
-  session:any
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${dmsans.className}`}>
-      <AppContextProvider>
-      <SessionProviderComp session={session}>
-        <ThemeProvider
-          attribute="class"
-          enableSystem={false}
-          defaultTheme="light"
-        >
-          <Aoscompo>
-            <Header />
-            <NextTopLoader />
-            {children}
-            <Footer />
-          </Aoscompo>
-          <ScrollToTop />
-        </ThemeProvider>
-        </SessionProviderComp>
+        <AppContextProvider>
+          <SessionProviderComp>
+            <ThemeProvider
+              attribute="class"
+              enableSystem={false}
+              defaultTheme="light"
+            >
+              <Aoscompo>
+                <Header />
+                <NextTopLoader />
+                {children}
+                <Footer />
+              </Aoscompo>
+              <ScrollToTop />
+            </ThemeProvider>
+          </SessionProviderComp>
         </AppContextProvider>
       </body>
     </html>
